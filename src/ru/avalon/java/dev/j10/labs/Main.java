2b9bf22f151c8201c69809ef2ccb01ca09f52c68
kiry <kiry@BENDER>
2020-10-29 16:39:41 +0300

    Lab_1_done

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,5 +1,7 @@
 package ru.avalon.java.dev.j10.labs;
 
+import ru.avalon.java.dev.j10.labs.commons.Address;
+import ru.avalon.java.dev.j10.labs.models.Passport;
 import ru.avalon.java.dev.j10.labs.models.Person;
 
 public class Main {
@@ -7,15 +9,12 @@ public class Main {
     /*
      * FIXME(Студент): Измените определение метода так, чтобы он стал точкой входа в приложение.
      */
-    Main() {
-
-        Person ivanov = null;
-        Person smith = null;
-
+    public static void main(String[] args) {
         /*
          * TODO(Студент): Создайте экземпляры класса 'Person'
-         *
-         * 1. Проинициализируейте переменную 'ivanov', так,
+         */
+        
+        /* 1. Проинициализируейте переменную 'ivanov', так,
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'Иванов Иван Иванович'.
          *
@@ -23,7 +22,15 @@ public class Main {
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'John Edvard Smith'.
          */
+       
+        Passport ivanovPassport = new Passport("4444 777777", "Иван", "Иванов", "Петрович", "04.05.1987", "05.06.2009", "УВД г. Москва" );
+        Address ivanovAddress = new Address("663300", "Россия", "Норильск", "Комсомольская", 3, 26);
+        Person ivanov = new Person(ivanovPassport, ivanovAddress);
 
+        Passport smithPassport = new Passport("5555 888888", "John", "Smith", "Edvard", "01.02.2002", " HM Government" );
+        Address smithAddress = new Address("AB10", "Great Britain", "Ferryhill", "Dean RD", 5, 33);
+        Person smith = new Person(smithPassport, smithAddress);
+   
         /*
          * TODO(Студент): Создайте несколько строковых переменных:
          *
@@ -43,6 +50,11 @@ public class Main {
          *    человека, описанного объектом, адресованным
          *    переменной 'smith'.
          */
+        String ivanovname = ivanov.getFullName();
+        String smithname = smith.getFullName();
+        String ivanovadr = ivanov.getAddress();
+        String smithadr = smith.getAddress();
+        
 
         /*
          * TODO(Студент): Выведите в консоль значения созданных строковых переменных:
@@ -50,5 +62,10 @@ public class Main {
          * Значение каждой переменной должно быть выведено на
          * отдельной строке.
          */
+        
+        System.out.println(ivanovname);
+        System.out.println(ivanovadr);
+        System.out.println(smithname);
+        System.out.println(smithadr);
     }
 }
